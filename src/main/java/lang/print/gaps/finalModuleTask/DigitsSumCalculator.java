20c84131cf92b3dbed7ce558a8a1b0b0bc6475dd
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-10 01:25:30 +0300

    Completed task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int one = number / 1000;
+        int two = (number - one*1000) / 100;
+        int three = (number - one*1000-two*100) / 10;
+        int four = number - one*1000-two*100-three*10;
+        System.out.println(one+two+three+four);
     }
 }
